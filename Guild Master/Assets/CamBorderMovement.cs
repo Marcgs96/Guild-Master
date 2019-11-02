a1f0bc4421af4efe2a689e99eb31f2845c080bfe
AxelAlavedra <axel.alavedra@gmail.com>
2019-11-02 20:50:29 +0100

    Camera very simple movement

---
+++ Guild Master/Assets/CamBorderMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBorderMovement : MonoBehaviour
+{
+    public float movement_speed;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+
+
+        if (x <= 0 + offset)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movement_speed * Time.deltaTime);      
+        if (x >= Screen.width - offset)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - movement_speed * Time.deltaTime);
+        if (y <= 0 + offset) 
+            transform.position = new Vector3(transform.position.x - movement_speed * Time.deltaTime, transform.position.y, transform.position.z);
+        if (y >= Screen.height - offset)
+            transform.position = new Vector3(transform.position.x + movement_speed * Time.deltaTime, transform.position.y, transform.position.z);
+       
+    }
+}
