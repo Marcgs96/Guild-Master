60ba47e0a5b62597a8b1d260e45b0377a4dbdb8d
AxelAlavedra <axel.alavedra@gmail.com>
2019-11-25 19:44:08 +0100

    Bottom panel. Guild and Blacksmith panels

--- Guild Master/Assets/GuildMaster/Scripts/UIManagement.cs
+++ Guild Master/Assets/GuildMaster/Scripts/UIManagement.cs
@@ -7,11 +7,27 @@ public class UIManagement : MonoBehaviour
     public GameObject quests_panel;
     public GameObject quests_list;
     public GameObject quest_preparation;
+    public GameObject guild_panel;
+    public GameObject blacksmith_panel;
 
 
     public void ActivateQuestsPanel()
     {
         quests_panel.SetActive(!quests_panel.activeSelf);
+        guild_panel.SetActive(false);
+        blacksmith_panel.SetActive(false);
+    }
+    public void ActivateGuildPanel()
+    {
+        guild_panel.SetActive(!guild_panel.activeSelf);
+        quests_panel.SetActive(false);
+        blacksmith_panel.SetActive(false);
+    }
+    public void ActivateBlacksmithPanel()
+    {
+        blacksmith_panel.SetActive(!blacksmith_panel.activeSelf);
+        guild_panel.SetActive(false);
+        quests_panel.SetActive(false);
     }
 
     public void OnQuestSelection()
@@ -23,6 +39,16 @@ public class UIManagement : MonoBehaviour
     public void CloseQuestsPanel()
     {
         quests_panel.SetActive(false);
+    }
+
+    public void CloseGuildPanel()
+    {
+        guild_panel.SetActive(false);
+    }
+
+    public void CloseBlacksmithPanel()
+    {
+        blacksmith_panel.SetActive(false);
     }
 
     public void CloseQuestPreparation()
