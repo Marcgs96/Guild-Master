13edee1712b819d0f8fa03c00ef90f4abfafed5e
AxelAlavedra <axel.alavedra@gmail.com>
2019-11-25 17:41:29 +0100

    Start of game UI. Quest Panel and resource panel

---
+++ Guild Master/Assets/GuildMaster/Scripts/Resource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource
+{
+    string name;
+    enum Resource_Type
+    {
+
+    }
+    Resource_Type type;
+}
---
+++ Guild Master/Assets/GuildMaster/Scripts/UIManagement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagement : MonoBehaviour
+{
+    public GameObject quests_panel;
+    public GameObject quests_list;
+    public GameObject quest_preparation;
+
+
+    public void ActivateQuestsPanel()
+    {
+        quests_panel.SetActive(!quests_panel.activeSelf);
+    }
+
+    public void OnQuestSelection()
+    {
+        quest_preparation.SetActive(true);
+        quests_list.SetActive(false);
+    }
+
+    public void CloseQuestsPanel()
+    {
+        quests_panel.SetActive(false);
+    }
+
+    public void CloseQuestPreparation()
+    {
+        quest_preparation.SetActive(false);
+        quests_list.SetActive(true);
+    }
+}
