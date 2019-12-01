fb5582c0a8111743b2b65644bc8fed18545f8921
AxelAlavedra <axel.alavedra@gmail.com>
2019-12-01 13:41:39 +0100

    Camera limits

--- Guild Master/Assets/GuildMaster/Scripts/ZoomScript.cs
+++ Guild Master/Assets/GuildMaster/Scripts/ZoomScript.cs
@@ -6,12 +6,18 @@ public class ZoomScript : MonoBehaviour
 {
     public float zoom_speed;
 
+    public float min_y = 5.0f;
+    public float max_y = 55.0f;
+
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
         pos.y -= Input.mouseScrollDelta.y * zoom_speed * Time.deltaTime;
         pos.x += Input.mouseScrollDelta.y * zoom_speed * Time.deltaTime;
-        transform.position = pos;
+
+
+        if (pos.y < max_y && pos.y > min_y)
+            transform.position = pos;
     }
 }
