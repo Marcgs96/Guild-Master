5371ef7fab7bf4e1eaaeb14017e7c66789c17010
AxelAlavedra <axel.alavedra@gmail.com>
2019-12-11 18:43:30 +0100

    New texts

--- Guild Master/Assets/ResourcePopUp.cs
+++ Guild Master/Assets/ResourcePopUp.cs
@@ -14,22 +14,22 @@ public class ResourcePopUp : MonoBehaviour
         switch (type)
         {
             case 1:
-                text_info = "Gold is a resource required to upgrade buildings such as the Guild House and the Blacksmith. It can also be used to buy upgrades for the heroes equipment. Obtainable via quests.";
+                text_info = "Gold is used to upgrade Buildings and level up Members. Obtainable from Quests.";
                 break;
             case 2:
-                text_info = "Crowns are a resource required to upgrade the Guildhall. Obtainable via quests.";
+                text_info = "Crowns are used to upgrade the Guild Hall. Obtainable from Quests.";
                 break;
             case 3:
-                text_info = "Shields are a resource required to upgrade the Blacksmith. Obtainable via quests.";
+                text_info = "Shields are used to upgrade the Blacksmith. Obtainable from Quests.";
                 break;
             case 4:
-                text_info = "Potions are a resource you can assign to a quest, allowing a member to heal 50% stamina when its stamina drops below 25%. Obtainable via mage work.";
+                text_info = "Potions allow a member to heal 50% stamina when it's stamina drops below 25%. Obtainable from Mage work.";
                 break;
             case 5:
-                text_info = "Meats are a resource you can assign to a quest, during the quest, a food check can happen, each member needs to eat 1 piece of meat, if there is no food left, stamina will decrease a 25% to each member who cant eat. Obtainable via hunter work.";
+                text_info = "Meat greatly increases the success chance of Adventure Quests. Obtainable from Hunter work.";
                 break;
             case 6:
-                text_info = "Flames are a resource you can assign to the quests to increase the rewards gained on succes. Obtainable via warrior work.";
+                text_info = "Flames greatly increase the success chance of Bounty Quests. Obtainable from Warrior work.";
                 break;
             default:
                 break;
